427980563a9f5338329c89e086c983f9ef9d7b77
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2024-06-11 21:11:43 +0530

    Time: 18 ms (61.00%) | Memory: 10.7 MB (39.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.length()==0 || t.length()==0 || s.length()<t.length())
-            return "";
- 
-        vector<int> v(256,0);
-        for(int i=0;i<t.length();++i)
-              v[t[i]]++;
- 
-        int start=0,end=0,ans_start=0;
-        int minlength=INT_MAX;
-        int count=0;
-        while(end<s.length())
-        {
-            if(v[s[end]]>0)
-                 count++;
- 
-            v[s[end]]--;
-            while(count==(int)t.length())//until the the window condition is satified keep shriking the window
-            {
-                if(minlength>end-start+1)
-                {
-                    minlength=end-start+1;//update the minimum length continuously
-                    ans_start=start; //starting index of the answer
-                }
- 
-                v[s[start]]++;
-                if(v[s[start]]>0)
-                    count--;
- 
-                start++;
-            }
-            end++;
+      
+
+        unordered_map<char, int> mp;
+        int minlen=INT_MAX, start=0;
+        for(auto ch:t){
+            mp[ch]++;
         }
- 
-        if(minlength==INT_MAX) //no string found
-            return "";
- 
-        return s.substr(ans_start,minlength);
+        int i=0,j=0,count=mp.size();
+        while(j<s.length()){
+            if(mp.find(s[j])!=mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]]==0)
+                    count--;
+            }
+            if(count==0){
+                while(count==0){
+                    if(mp.find(s[i])!=mp.end()){
+                        mp[s[i]]++;
+                        if(mp[s[i]]==1) {
+                            count++;
+                            if(j-i+1<minlen){
+                                minlen=j-i+1;
+                                start=i;
+                            }
+                        }
+                    }
+                    i++;
+                }  
+            }
+            j++;
+        }
+        if(minlen==INT_MAX) return "";
+        return s.substr(start, minlen);
     }
 };
