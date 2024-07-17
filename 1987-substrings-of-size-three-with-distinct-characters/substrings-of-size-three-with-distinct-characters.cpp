568aeb8cd67dccaba0e90657f033bd7e10eba7e1
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2024-07-17 19:48:51 +0530

    Time: 6 ms (25.46%) | Memory: 9.4 MB (20.61%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
    int countGoodSubstrings(string s) {
-        if (s.size() < 3) return 0;
+        // if (s.size() < 3) return 0;
 
-        unordered_map<char, int> mpp;
-        int i = 0, j = 0, cnt = 0;
+        // unordered_map<char, int> mpp;
+        // int i = 0, j = 0, cnt = 0;
         
-        while (j < s.size()) {
-            mpp[s[j]]++;
+        // while (j < s.size()) {
+        //     mpp[s[j]]++;
             
-            if (j - i + 1 == 3) {
-                // Check if we have 3 distinct characters
-                if (mpp.size() == 3) {
-                    cnt++;
+        //     if (j - i + 1 == 3) {
+        //         // Check if we have 3 distinct characters
+        //         if (mpp.size() == 3) {
+        //             cnt++;
+        //         }
+        //         // Slide the window
+        //         mpp[s[i]]--;
+        //         if (mpp[s[i]] == 0) {
+        //             mpp.erase(s[i]);
+        //         }
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        
+        // return cnt;
+        int n=s.length();
+        int i=0,j=0,count=0;
+        unordered_map<char,int>mpp;
+        while(j<n)
+        {
+            mpp[s[j]]++;
+            if(j-i+1==3)
+            {
+                if(mpp.size()==3)
+                {
+                    count++;
                 }
-                // Slide the window
                 mpp[s[i]]--;
-                if (mpp[s[i]] == 0) {
+                if(mpp[s[i]]==0)
+                {
                     mpp.erase(s[i]);
                 }
                 i++;
             }
             j++;
         }
-        
-        return cnt;
+        return count;
     }
 };
