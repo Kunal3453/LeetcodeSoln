90cb61579b53a275741200345ff6adc205a48679
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2024-09-10 10:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (98.26%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int ans=0;
+        int n=citations.size();
+        for(int i=0;i<n;i++)
+        {
+            if(citations[i]>=n-i)
+            {
+ans=max(ans,n-i);
+            }
+        }
+        return ans;
+    }
+};
